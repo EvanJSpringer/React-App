59b442132705fbfa038a45456bc2dff7f8bd0db8
EvanJSpringer <81826544+EvanJSpringer@users.noreply.github.com>
2022-09-28 13:27:25 -0700

    frontend linking base

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,18 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Table from './Table'
 import Form from './Form';
 
-
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetchAll().then( result => {
+       if (result)
+          setCharacters(result);
+     });
+ }, [] );
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
     </div>
   )
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result && result.status === 200)
+      setCharacters([...characters, person] );
+    });
   }
   function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
@@ -20,6 +29,28 @@ function MyApp() {
       });
       setCharacters(updated);
   }
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:5000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+  }
+  async function makePostCall(person){
+    try {
+      const response = await axios.post('http://localhost:5000/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  
 }
 
 export default MyApp;
